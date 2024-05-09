55b37825bb9d83f0b92258370c72943d22b27759
den.akifev <den.akifev@mail.ru>
2024-05-09 12:34:01 +0300

    CI test

--- src/test/java/operations/operationsTest.java
+++ src/test/java/operations/operationsTest.java
@@ -14,23 +14,23 @@ public class operationsTest {
 
   @Test
   public void _max() {
-    assertEquals(100, operations._max("samples/test1.txt"));
-    assertEquals(40, operations._max("samples/test2.txt"));
-    assertEquals(50, operations._max("samples/test3.txt"));
+    assertEquals(10, operations._max("samples/test1.txt"));
+    assertEquals(4, operations._max("samples/test2.txt"));
+    assertEquals(5, operations._max("samples/test3.txt"));
   }
 
   @Test
   public void _sum() {
-    assertEquals(-100, operations._sum("samples/test1.txt"));
-    assertEquals(-210, operations._sum("samples/test2.txt"));
-    assertEquals(-260, operations._sum("samples/test3.txt"));
+    assertEquals(-10, operations._sum("samples/test1.txt"));
+    assertEquals(-21, operations._sum("samples/test2.txt"));
+    assertEquals(-26, operations._sum("samples/test3.txt"));
   }
 
   @Test
   public void _mult() {
-    assertEquals(-720000, operations._mult("samples/test1.txt"));
-    assertEquals(00, operations._mult("samples/test2.txt"));
-    assertEquals(4725000, operations._mult("samples/test3.txt"));
+    assertEquals(-72000, operations._mult("samples/test1.txt"));
+    assertEquals(0, operations._mult("samples/test2.txt"));
+    assertEquals(472500, operations._mult("samples/test3.txt"));
 
   }
 
@@ -40,8 +40,8 @@ public class operationsTest {
     double test2Res = (operations._min("samples/test2.txt") + operations._max("samples/test2.txt")) / 2.0d;
     double test3Res = (operations._min("samples/test3.txt") + operations._max("samples/test3.txt")) / 2.0d;
     assertEquals(0, test1Res, 1e-8);
-    assertEquals(-30, test2Res, 1e-8);
-    assertEquals(-2.50, test3Res, 1e-8);
+    assertEquals(-3, test2Res, 1e-8);
+    assertEquals(-2.5, test3Res, 1e-8);
   }
 
 }
